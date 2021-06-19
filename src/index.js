ea186e3c585fe8780dfcdccf7e96737ea1ee0d25
Iryna Kolesnyk <80821846+IrynaKolesnyk@users.noreply.github.com>
2021-06-19 21:52:07 +0200

    menu cards markup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,27 @@
 'use strict';
+import menuCardTemplate from "./templates/menu-card.hbs";
+import menu from "./menu.json";
+
 
 const Theme = {
     LIGHT: 'light-theme',
     DARK: 'dark-theme',
 };
+
+const refs = {
+    menuContainer: document.querySelector('.js-menu'),
+    // btnAddToBasket: document.querySelector('menu .card__button')
+};
+
+
+
+// создание разметки меню
+const cardsMenu = createMenuCardsMarkup(menu);
+refs.menuContainer.insertAdjacentHTML('beforeend', cardsMenu);
+
+function createMenuCardsMarkup(menu) {
+    return menu.map(item => menuCardTemplate(item)).join('');
+    // аналог записи return menu.map(menuCardTemplate).join('');
+};
+
+// смена темы сайта
